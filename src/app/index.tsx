b440f0599357c20c0a54c98a9443be898a0e76d8
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2025-10-28 22:47:31 -0300

    Add Components
    Add Button, CurrencyInput, Input, List and PageHeader.
    Update TargetPage and home

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,9 @@
 import { HomeHeader } from "@/components/HomeHeader";
 import { Target } from "@/components/Target";
-import { View } from "react-native";
+import { StatusBar, View } from "react-native";
+import { List } from "@/components/List";
+import { Button } from "@/components/Button";
+import { router } from "expo-router";
 
 const summary = {
   total: "R$ 19.600,00",
@@ -10,13 +13,22 @@ const summary = {
 
 const targets = [
   {
+    id: "321",
     name: "Apartamento",
     current: "R$ 9.000,00",
     percentage: "45%",
     target: "R$ 20.000,00",
   },
   {
+    id: "123",
     name: "Viagem",
+    current: "R$ 10.000,00",
+    percentage: "50%",
+    target: "R$ 20.000,00",
+  },
+  {
+    id: "1235",
+    name: "Carro",
     current: "R$ 10.000,00",
     percentage: "50%",
     target: "R$ 20.000,00",
@@ -25,13 +37,27 @@ const targets = [
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-      }}>
+    <View style={{ flex: 1 }}>
+      <StatusBar barStyle="light-content" />
       <HomeHeader data={summary} />
 
-      <Target data={targets[0]} />
+      <List
+        data={targets}
+        renderItem={({ item }) => (
+          <Target
+            data={item}
+            onPress={() => router.navigate(`/in-progress/${item.id}`)}
+          />
+        )}
+        title="Metas"
+        keyExtractor={(item) => item.id}
+        emptyMessage="Nenhuma meta. Clique em nova meta para criar."
+        containerStyle={{ paddingHorizontal: 24 }}
+      />
+
+      <View style={{ padding: 24, paddingBottom: 32 }}>
+        <Button title="Nova meta" onPress={() => router.navigate("/target")} />
+      </View>
     </View>
   );
 }
--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,11 +1,26 @@
-import { Text, View, Button } from "react-native";
-import { router } from "expo-router";
+import { View } from "react-native";
+import { PageHeader } from "@/components/PageHeader";
+import { Input } from "@/components/Input";
+import { Button } from "@/components/Button";
+import { CurrencyInput } from "@/components/CurrencyInput";
 
 export default function Target() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Target</Text>
-      <Button title="Voltar" onPress={() => router.back()} />
+    <View style={{ flex: 1, padding: 24 }}>
+      <PageHeader
+        title="Meta"
+        subtitle="Economize para alcançar sua meta financeira."
+      />
+      <View style={{ marginTop: 32, padding: 24 }}>
+        <Input
+          label="Nome da meta"
+          placeholder="Ex: Entrada para apartamento, Viagem para praia"
+        />
+
+        <CurrencyInput label="Valor Alvo" value={0} />
+
+        <Button title="Salvar" />
+      </View>
     </View>
   );
 }
